4239d634e877a1e7d6e6e8bc1a63491b1d8c109d
patorma <pocontreras@ing.ucsc.cl>
2018-08-13 02:56:21 -0300

    Se dibujo con javascript usando canvas un triangulo de lineas roja

---
+++ canvas2.js
@@ -0,0 +1,35 @@
+//1. Obtener el elemento canvas del html
+var canvas = document.getElementById("mycanvas");
+//2 .Obtener el contexto
+
+var c = canvas.getContext("2d");
+//3. Entender el sistema de coordenadas (x,y)
+ //4. A dibujar
+
+
+//Lineas
+
+//c.moveTo(250,250);
+//c.lineTo(0,500);//destino final de la linea
+//c.lineTo(500,250);
+//c.lineTo(250,250);
+//c.stroke();//dibuja
+
+//c.beginPath();//cambiar de ruta o posicion de coordenadas
+//c.moveTo(0,0);
+//c.lineTo(200,200);
+//c.strokeStyle = "green";
+//c.lineWidth = 20;
+//c.stroke();
+var w = canvas.width;
+var h = canvas.height;
+//alert(w+h+10);
+c.beginPath();
+c.moveTo(w/2,h/2);
+c.lineTo(w*0.75,h*0.75);
+c.lineTo(w*0.25,h*0.75);
+c.closePath();//cierra segmentos(une el primer segmento con el ultimo)
+c.strokeStyle = "red";
+c.lineWidth = 5;
+c.stroke();
+
