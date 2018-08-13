95b949cffd657095d659cbba3fbcbaa2d4ec645b
patorma <pocontreras@ing.ucsc.cl>
2018-08-13 02:55:11 -0300

    Archivo js para introducción de ciertos comandos en javascript

---
+++ canvas.js
@@ -0,0 +1,29 @@
+//Archivo para dibujar canvas
+
+//Variables
+var profesor = "Patricio Contreras"; //set
+
+
+//Tipos de datos
+var estudiante = "Carlos";//string
+var edad =28; //numero
+var casado = false; //boolean (true|false)
+
+//Los objetos tienen propiedades y métodos 
+//Las propiedades son caracteristicas
+//Los métodos son funciones (hacen algo)
+//Ejemplo (para el objeto carro) 
+//var carro = new Object();
+carro.color = "red"; //propiedad
+carro.year = 2018; //propiedad
+carro.acelerar(); //método
+
+//get (obtener) y set (definir)
+window.innerWidth(); //get 
+
+/*function sumar(a,b){
+    return a + b;
+}*/
+
+//sumar(5,7);
+//console.log("El valor de la suma de "+a+"y"+b +"es: "+sumar(5,7));
